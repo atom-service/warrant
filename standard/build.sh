e8ae735f69deb2e855b7515282d91388bb55d35a
yinxulai <yinxulai@hotmail.com>
2019-07-15 09:28:11 +0800

    first commit

---
+++ standard/build.sh
@@ -0,0 +1,20 @@
+
+#！/bin/bash
+
+# 获取脚本的执行目录
+BasePath=$(cd `dirname $0`; pwd)
+ProtoPath=$(cd `dirname $0`; pwd)
+
+## 向外输出
+echo "[  工作目录  ]:  ${BasePath}"
+echo "[ ProtoPath ]:  $ProtoPath"
+
+# 进入工作目录
+cd $BasePath
+
+protoc  --proto_path=.  --go_out=plugins=grpc:.  *.proto
+
+protoc  --letmegrpc_out=. *.proto
+mv ./standard.letmegrpc.go  ./standard.test.go
+
+echo "[  完成  ]"
